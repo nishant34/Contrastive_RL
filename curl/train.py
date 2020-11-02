95d305f59b2b4f9ddcabd7408f621ce4e9d238d2
nishant jain <njain@cs.iitr.ac.in>
2020-11-02 20:47:13 +0530

    Update train.py

--- curl/train.py
+++ curl/train.py
@@ -9,9 +9,11 @@ from common import *
 from video_object import *
 from torchvision import transforms
 from Actor_critic import *
+from torch.utils.tensorboard import SummaryWriter
 
 
 def main():
+    
     env = dmc2gym.make(domain_name=domain_name,task_name =task_name,seed = seed,
     visualize_reward=False,from_pixels=1,height=initial_image_size,width=initial_image_size)
     env.seed(seed1)
@@ -25,41 +27,54 @@ def main():
     initial_obs_shape = (3*num_frame_stack,initial_image_size,initial_image_size)
     replay_buffer = Replay_buffer(capacity,batch_size)
     agent = final_agent(obs_shape,action_shape,hidden_dim,encoder_feature_dim,curl_latent_dim)
-
-    episode,episode_reward,done = 0,0,True
+    if not os.path.isdir(tensorboard_dir):
+      os.mkdir(tensorboard_dir)
+    writer  = SummaryWriter(tensorboard_dir)
     
+    
+    episode,episode_reward,done = 0,0,True
+    prev_episode = 0
+    curr_loss = 0
+    best_reward = 0
+    num_steps = 0
     for step in range(num_epochs):
-        print(step)
+        #print(step)
         #if steps%eval_steps==0:
-
+        num_steps +=1
 
         if done==True:
-
+            average_reward = episode_reward/num_steps
+            print("______________________________________________________________________________________________________________________")
+            print("Current Episode:{}".format(episode),"||","average_reward:{}".format(episode_reward/num_steps),"||","best_reward:{}".format(best_reward),"||","Current loss:{}".format(curr_loss),"||","number of steps in episode:{}".format(num_steps))
             obs = env.reset()
             next_obs = env.reset()
             done =False
-            reward=0
+            num_steps = 0
+            episode_reward=0
+            reward = 0
+            best_reward = 0
             episode_step=0
+            prev_episode = episode
             episode+=1
             action = env.action_space.sample()
             replay_buffer.append(make_tuple(obs,action,reward,next_obs,done))
 
         if step<initial_required_steps:
           action = env.action_space.sample()
-          print(type(action))
+          #print(type(action))
           #print("initial_actions.format:{}",action)
         else:
           action = agent.select_action(obs)
           #action1 = action[0]
-          print(action.shape)
-          print(action)
-          print(type(action))
+          #print(action.shape)
+          #print(action)
+          #print(type(action))
           #print("actions_now.format:{}",action)
            
         #print(type(action))
         #print(action)
         #action = np.array(action)
-        if step<initial_required_steps:
+        if step>initial_required_steps:
             #update_weights()
           loss1 = actor_loss(agent,obs)
           params1 = agent.actor.parameters()
@@ -80,8 +95,10 @@ def main():
           opt3.step()
            
 
-
+          curr_loss = loss1 + loss2 + loss3
         next_obs,reward,done,_ = env.step(action)
+        if reward>best_reward:
+          best_reward = reward
         #done = 0 if episode_step+1==env.max_episode_steps    
 
         episode_reward+=reward
@@ -89,6 +106,17 @@ def main():
 
         obs = next_obs
         step+=1
+        writer.add_scalar("Average  Reward",episode_reward/(num_steps+1),episode)
+        writer.add_scalar("Best  Reward",best_reward,episode)
+        writer.add_scalar("Cumulative Loss",curr_loss,episode)
+        
+        if episode%10==0:
+         if prev_episode!=episode:
+          print("Saving Model............")
+          current_ep = format(episode,'04')
+          torch.save(agent.state_dict(),'model_' + current_ep + '.pth' )
+          print("model has been saved")
+          prev_episode = episode
 
 
 if __name__=="__main__":
